327c5ebfc4d3df3c7bff462a49dd86ea440326bd
RanyellHenrique <ranyellhenrique@gmail.com>
2020-09-29 15:57:22 -0300

    Project created

---
+++ src/main/java/com/bootcamp/BootcampChapter1/BootcampChapter1Application.java
@@ -0,0 +1,13 @@
+package com.bootcamp.BootcampChapter1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampChapter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampChapter1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp/BootcampChapter1/BootcampChapter1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.BootcampChapter1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampChapter1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
